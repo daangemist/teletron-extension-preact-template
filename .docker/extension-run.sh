35c047c30968384b59dae12e4b1a5405a83c43c4
daangemist <git@daangemist.nl>
2022-09-04 19:43:10 +0200

    Updated extension-run.sh to also install parent folder.

--- .docker/extension-run.sh
+++ .docker/extension-run.sh
@@ -2,8 +2,16 @@
 
 set -e
 
+CURRENT=$PWD
 if [ ! -d "$PWD/node_modules" ]; then
     npm ci
 fi
+
+# If the parent folder is not installed yet, install it.
+if [ ! -d "$PWD/../node_modules" ]; then
+    cd .. && npm ci
+fi
+
+cd $CURRENT
 
 npm run watch
